c0239a0036756bc4444d511e44505d73a0cc242b
Anxton <anxton4@gmail.com>
2024-06-14 07:32:50 +0200

    working

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import stable_marriage as sm, json
 
+VERBOSE = True
 
-if __name__ == "__main__":    
-    f = open('data.json')
+if __name__ == "__main__":
+    f = open("data.json")
 
     data = json.load(f)
 
     # main program
-    print("Welcome to the School Allocation System\n")
+    print("Welcome to the School Allocation System")
     answer = 0
     while answer not in range(1, 3):
         try:
             print("Please select an option from the following:")
+            print("-------------------------------------------")
             print("1. Students serenade schools")
             print("2. Schools serenade students")
             answer = int(input("Enter your choice: "))
-        except ValueError:
-            print("Please enter a valid choice\n")
+        except:
             continue
-    
-    if answer == 1:
-        serenading = "students"
-    elif answer == 2:
-        serenading = "schools"
 
-    stable_marriage = sm.StableMarriage(data, serenading=serenading)
+    if answer == 1:
+        serenading = "student"
+    elif answer == 2:
+        serenading = "school"
+
+    stable_marriage = sm.StableMarriage(data, serenader=serenading, verbose=VERBOSE)
 
     stable_marriage.run()
 
+    rounds = stable_marriage.get_rounds()
     schools = stable_marriage.get_schools()
     unmatched_students = stable_marriage.get_unmatched_students()
-    rounds = stable_marriage.get_rounds()
 
-
-    print(f"School Allocation completed in {rounds} rounds")
-    print("The following students have been allocated to the following schools:\n")
+    print(f"\nSchool Allocation completed in {rounds} rounds\n")
+    print("The following students have been allocated to the following schools:")
     for school in schools:
-        print(f"{school.name}: {school.students}\n")
+        print(
+            f"{school.name} ({len(school.students)}/{school.capacity}):\n {school.students}"
+        )
 
-    print("The following students could not be allocated to any school:\n")
-    for student in unmatched_students:
-        if student.school is None:
-            print(student.name)
+    print()
+    if len(unmatched_students) == 0:
+        print("All students have been allocated to a school")
+    else:
+        print("The following students could not be allocated to any school:")
+        print(unmatched_students)
 
     f.close()
--- stable_marriage.py
+++ stable_marriage.py
@@ -1,150 +1,280 @@
-class StableMarriage:
-    def __init__(self, data, serenading="student", verbose=False):
-        self.serenaders = serenading
-        self.rounds = 0
-        if self.serenaders not in {"student", "school"}:
-            raise ValueError("serenading must be either 'student' or 'school'")
-        
-        if self.serenaders == "student":
-            # students serenade schools
-            # init students as a set of Serenaders
-            self.serenaders = {Serenader(student['name'], student['preferences']) for student in data['students']}
+class School:
+    def __init__(self, name, students, capacity):
+        self.name = name
+        self.students = students
+        self.capacity = capacity
 
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+
+class StableMarriage:
+    def __init__(self, data, serenader="student", verbose=False):
+        self.serenader = serenader
+        self.rounds = 0
+        if self.serenader not in {"student", "school"}:
+            raise ValueError("serenading must be either 'student' or 'school'")
+
+        if self.serenader == "student":
+            # students serenade schools
             # init schools as a set of Serenadees
-            self.serenadees = {Serenadee(school['name'], school['preferences'], school['capacity']) for school in data['schools']}
+            self.serenadees = {
+                Serenadee(
+                    school["name"],
+                    school["preferences"],
+                    school["capacity"],
+                    verbose=verbose,
+                )
+                for school in data["schools"]
+            }
+
+            # init students as a set of Serenaders
+            self.serenaders = {
+                Serenader(student["name"], student["preferences"])
+                for student in data["students"]
+            }
         else:
             # schools serenade students
             # init students as a set of Serenadees
-            self.serenadees = {Serenadee(student['name'], student['preferences']) for student in data['students']}
+            self.serenadees = {
+                Serenadee(student["name"], student["preferences"], verbose=verbose)
+                for student in data["students"]
+            }
 
             # init schools as a set of Serenaders
-            self.serenaders = {Serenader(school['name'], school['preferences'], school['capacity']) for school in data['schools']}
+            self.serenaders = {
+                Serenader(school["name"], school["preferences"], school["capacity"])
+                for school in data["schools"]
+            }
 
         # debugging
         self.verbose = verbose
 
-    def all_serenaders_matched(self):
-        for serenader in self.serenaders:
-            if serenader.is_free():
-                return False
-        return True
+    def get_rounds(self):
+        return self.rounds
 
-    def all_serenadees_full(self):
-        for serenadee in self.serenadees:
-            if not serenadee.is_full():
-                return False
-        return True
+    def get_schools(self):
+        if self.serenader == "student":
+            return {
+                School(
+                    serenadee.name,
+                    [Student(s.name) for s in serenadee.matched_serenaders],
+                    serenadee.capacity,
+                )
+                for serenadee in self.serenadees
+            }
+        else:
+            return {
+                School(
+                    serenader.name,
+                    [Student(s.name) for s in serenader.matched],
+                    serenader.capacity,
+                )
+                for serenader in self.serenaders
+            }
+
+    def get_unmatched_students(self):
+        if self.serenader == "student":
+            return {
+                Student(serenader.name)
+                for serenader in self.serenaders
+                if not serenader.is_fulfilled()
+            }
+        else:
+            return {
+                Student(serenadee.name)
+                for serenadee in self.serenadees
+                if not serenadee.is_fulfilled()
+            }
+
+    def all_serenaders_fulfilled(self):
+        return all(serenader.is_fulfilled() for serenader in self.serenaders)
+
+    def all_serenadees_fulfilled(self):
+        return all(serenadee.is_fulfilled() for serenadee in self.serenadees)
 
     def run(self):
         # exit conditions:
-        # - all serenaders are matched
-        #   - schools: all schools are not free -> all schools are full
-        #   - students: all students are not free -> all students have a school
-        # - all serenadees are full
+        # - all serenaders are fulfilled
+        #   - schools: all schools are full
+        #   - students: all students have found a school
+        # - all serenadees are fulfilled
         #   - students: all students have found a school
         #   - schools: all schools are full
-        while not (self.all_serenaders_matched() or self.all_serenadees_full()):
+        while (
+            not self.all_serenaders_fulfilled() and not self.all_serenadees_fulfilled()
+        ):
             # beginning of round/day
             self.rounds += 1
             if self.verbose:
-                print(f"Round {self.rounds}")
+                print()
                 print("---------------------------------")
+                print(f"Round {self.rounds} begins")
 
-            # morning: serenaders go serenade serenadees
-            round_serenadees = set() # serenadees that are serenaded in the current round
+            # morning: serenaders go serenade who they like
+            if self.verbose:
+                print("--------")
+                print("Morning:")
+                print("--------")
             for serenader in self.serenaders:
                 if self.verbose:
-                    print(f"{serenader.name} serenades:")
-
-                # serenade as long as the serenader is free and has preferences
-                while
-
-                if not serenader.is_free():
-                    # serenader is already matched: school is full or student has a school
-                    if self.verbose:
-                        print(f"{serenader.name} is already matched, and keeps serenading")
-                    continue
-
-                if not serenader.has_next_preference():
-                    # free serenader has no more preferences, should be impossible since it would mean serenadees are all full and thus would have stopped the loop
-                    if self.verbose:
-                        print(f"{serenader.name} has no more preferences")
-                    continue
-
-                # get the next serenadee to serenade
-                serenadee_name = serenader.get_next_preference()
-                serenaded = next(serenadee for serenadee in self.serenadees if serenadee.name == serenadee_name)
-
-                round_serenadees.add(serenaded)
-                serenaded.serenaders.add(serenader)
+                    print(f"- {serenader.name} serenades:")
 
                 if self.verbose:
-                    print(f"{serenader.name} serenades {serenaded.name}")
-                    print("---------------------------------")
+                    # these checks are not necessary and are already accounted for in "serenader.pop_next_preferences()"
 
-            # afternoon/evening: serenadees keep the n = capacity most preferred serenaders and kicks the rest
-            for serenadee in round_serenadees:
-                serenadee.keep_and_kick_serenaders()
+                    if serenader.is_fulfilled():
+                        print(f"{serenader.name} is already fulfilled")
+
+                    if len(serenader.preferences) == 0:
+                        print(f"{serenader.name} has no more preferences")
+
+                # serenade the n=capacity first preferences
+                for serenadee_name in serenader.pop_next_preferences():
+                    # find the serenadee from its name
+                    serenadee = next(
+                        serenadee
+                        for serenadee in self.serenadees
+                        if serenadee.name == serenadee_name
+                    )
+                    serenadee.new_serenaders.add(serenader)
+
+                    if self.verbose:
+                        print(f"{serenader.name} serenades {serenadee.name}")
+
+            # afternoon/evening: serenadees match the n=capacity most preferred serenaders and rejects the rest
+            if self.verbose:
+                print("------------------")
+                print("Afternoon/Evening:")
+                print("------------------")
+            for serenadee in self.serenadees:
+                # match and reject serenaders for serenadees that have new serenaders
+                if len(serenadee.new_serenaders) > 0:
+                    if self.verbose:
+                        print(f"- {serenadee.name} matches and rejects serenaders:")
+                    serenadee.match_and_reject()
+
+            if self.verbose:
+                print()
+                print(f"Round {self.rounds} ends")
+                print("---------------------------------")
+
 
 class Serenadee:
-    def __init__(self, name, preferences, capacity=1):
+    def __init__(self, name, preferences, capacity=1, verbose=False):
         self.name = name
         self.preferences = preferences
         self.capacity = capacity
+        self.verbose = verbose
 
-        self.serenaders = set()
-        """serenaders in the midst of a given round"""
-        self.matched = set()
-        """serenaders who are temporarily matched to the serenadee"""
+        self.new_serenaders = set()
+        """new serenaders from a round"""
+        self.matched_serenaders = []
+        """serenaders who are temporarily matched to the serenadee (and that are carried over to the next round)"""
 
-    def is_full(self):
-        return len(self.matched) >= self.capacity
+    def is_fulfilled(self):
+        return len(self.matched_serenaders) >= self.capacity
 
-    def keep_and_kick_serenaders(self):
-        '''
-        Match serenaders in `self.serenaders` to the serenadee.\n
-        Adds `self` to `serenader.matched` and add the serenadee to `self.matched`.
-        '''
-        if (
-            self.capacity == 0
-            or len(self.serenaders) == 0
-            or self.is_full()
-        ):
+    def match_and_reject(self):
+        """
+        Match serenaders to the serenadee, given the serenadee's preferences.\n
+
+        Match the first `self.capacity` serenaders and reject the rest.\n
+        Matched serenaders are added to `self.matched_serenaders` and have their `matched` updated to include `self`.\n
+        Rejected serenaders have their `matched` updated to remove `self`.
+        """
+        if len(self.new_serenaders) == 0:
+            # no new serenaders this round
+            # shouldn't happen since this function is only called on serenadees with new serenaders
             if self.verbose:
-                print(f"{self.name} is full or has no serenaders to match")
+                print(f"{self.name} has no new serenaders this round")
             return
-        
-        for serenader in self.serenaders:
-            if serenader in self.matched:
-                # serenader is already matched to the serenadee, should be impossible since serenaders are removed from serenadee.serenaders after being matched
+
+        if self.verbose:
+            print(f"current serenaders for {self.name}: {self.matched_serenaders}")
+            print(f"new serenaders for {self.name}: {self.new_serenaders}")
+
+        # all serenaders that are matched to the serenadee (temporarily over capacity)
+        self.matched_serenaders = list(
+            set(self.matched_serenaders).union(self.new_serenaders)
+        )
+        self.new_serenaders.clear()
+
+        if len(self.matched_serenaders) <= self.capacity:
+            # all new serenaders can be matched
+            if self.verbose:
+                print(f"enough capacity: all serenaders can be matched to {self.name}")
+            for serenader in self.matched_serenaders:
+                serenader.matched.add(self)
+        else:
+            # not all new serenaders can be matched
+            # match the first `self.capacity` preferred serenaders
+            if self.verbose:
+                print(
+                    f"not enough capacity: only the first {self.capacity} preferred serenaders can be matched to {self.name}"
+                )
+
+            # sort the serenaders by their preference (index in the preferences list)
+            self.matched_serenaders.sort(
+                key=lambda serenader: self.preferences.index(serenader.name)
+            )
+
+            if self.verbose:
+                print(f"sorted serenaders by preference: {self.matched_serenaders}")
+
+            # reject the serenaders that are not in the first `self.capacity`
+            for serenader in self.matched_serenaders[self.capacity :]:
+                serenader.matched.discard(self)
+
                 if self.verbose:
-                    print(f"{serenader.name} is already matched to {self.name}")
-                continue
+                    print(f"{serenader.name} is rejected from {self.name}")
 
-            if len(self.matched) >= self.capacity:
-                # serenadee is full
-                if self.verbose:
-                    print(f"{self.name} is full: {serenader.name} is not matched")
-                break
+            # delete the rejected serenaders
+            del self.matched_serenaders[self.capacity :]
 
-            self.matched.add(serenader)
-            serenader.matched.add(self)
+            # match the first `self.capacity` serenaders
+            for serenader in self.matched_serenaders:
+                serenader.matched.add(self)
 
-        self.serenaders = set()
+        if self.verbose:
+            print(f"matched serenaders for {self.name}: {self.matched_serenaders}")
+
 
 class Serenader:
     def __init__(self, name, preferences, capacity=1):
         self.name = name
         self.preferences = preferences
+        """
+        List of preferences from most to least preferred.\n
+        The preferences are the names of the serenadees.\n
+        When a preference is gotten, it is removed from this list.
+        """
+
         self.capacity = capacity
 
         self.matched = set()
 
-    def is_free(self):
-        return len(self.matched) <= self.capacity
+    def is_fulfilled(self):
+        return len(self.matched) >= self.capacity
 
-    def get_next_preference(self):
-        return self.preferences.pop(0)
+    def available_capacity(self):
+        return self.capacity - len(self.matched)
 
-    def has_next_preference(self):
-        return len(self.preferences) > 0
+    def pop_next_preferences(self):
+        """
+        Get the next preferences to serenade that the serenader has not serenaded yet.\n
+        There will be `min(len(self.preferences), self.available_capacity())` preferences returned.\n
+        They are removed from `self.preferences`.\n
+        """
+        available_capacity = self.available_capacity()
+        result = self.preferences[:available_capacity]
+        del self.preferences[:available_capacity]
+        return result
+
+    def __repr__(self):
+        return self.name
